8bd758315ac0df4d73c51fe586aa6b36541a58ac
kabitaram007 <kabitaram007@gmail.com>
2021-12-11 11:45:11 +0530

    upload 111221-2
    upload 111221-2
    
    Co-Authored-By: Samir Ram <samirram007@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\ServiceController as SC;
 use App\Http\Controllers\CustomerController as CU;
 use App\Http\Controllers\PatientController as PU;
 use App\Http\Controllers\ProductController as PC;
+use App\Http\Controllers\EmployeeController as EC;
+use App\Http\Controllers\RefDoctorController as RC;
+use App\Http\Controllers\AgencyController as AC;
+use App\Http\Controllers\OutdoorDoctorController as OC;
+use App\Http\Controllers\ReportingDoctorController as RU;
+
  
 
 // Route::get('/welcome', function () {
@@ -46,9 +52,30 @@ Route::group(['middleware'=>'preventbackhistory'],function(){
         Route::get('/addcustomer',[CU::class, 'CustomerAddModal'])->name('addcustomer');
         Route::post('/customer-save',[CU::class, 'saveCustomer'])->name('save-customer');
 
-        Route::get('/patient', [CU:: class, 'getPatientList'])->name('patient-list');
-        Route::get('/addpatient',[CU::class, 'PatientAddModal'])->name('addpatient');
-        Route::post('/patient-save',[CU::class, 'savePatient'])->name('save-patient');
+        Route::get('/patient', [PU:: class, 'getPatientList'])->name('patient-list');
+        Route::get('/addpatient',[PU::class, 'PatientAddModal'])->name('addpatient');
+        Route::post('/patient-save',[PU::class, 'savePatient'])->name('save-patient');
+
+        Route::get('/agency', [AC:: class, 'getAgencyList'])->name('agency-list');
+        Route::get('/addagency',[AC::class, 'AgencyAddModal'])->name('addagency');
+        Route::post('/agency-save',[AC::class, 'saveAgency'])->name('save-agency');
+
+        Route::get('/employee', [EC:: class, 'getEmployeeList'])->name('employee-list');
+        Route::get('/addemployee',[EC::class, 'EmployeeAddModal'])->name('addemployee');
+        Route::post('/employee-save',[EC::class, 'saveEmployee'])->name('save-employee');
+
+        Route::get('/outdoor_doctor', [OC:: class, 'getOutdoor_doctorList'])->name('outdoor_doctor-list');
+        Route::get('/addoutdoor_doctor',[OC::class, 'Outdoor_doctorAddModal'])->name('addoutdoor_doctor');
+        Route::post('/outdoor_doctor-save',[OC::class, 'saveOutdoor_doctor'])->name('save-outdoor_doctor');
+
+        Route::get('/ref_doctor', [RC:: class, 'getRef_doctorList'])->name('ref_doctor-list');
+        Route::get('/addref_doctor',[RC::class, 'Ref_doctorAddModal'])->name('addref_doctor');
+        Route::post('/ref_doctor-save',[RC::class, 'saveRef_doctor'])->name('save-ref_doctor');
+
+        Route::get('/reporting_doctor', [RU:: class, 'getReporting_doctorList'])->name('/reporting_doctor-list');
+        Route::get('/addreporting_doctor',[RU::class, 'Reporting_doctorAddModal'])->name('addreporting_doctor');
+        Route::post('/reporting_doctor-save',[RU::class, 'saveReporting_doctor'])->name('save-reporting_doctor');
+
 
         Route::get('/service',[SC::class, 'getServiceList'])->name('service-list');
         Route::get('/addService',[SC::class, 'ServiceAddModal'])->name('addservice');
